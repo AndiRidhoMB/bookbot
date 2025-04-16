e41bc0b10634762ca4e1f10b4b13b2bf7f9b1419
Ridho M <ridho.mb25@gmail.com>
2025-04-17 04:34:03 +0700

    creating code to count words and each characters

---
+++ main.py
@@ -0,0 +1,17 @@
+from stats import get_num_words, count_characters
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_characters = count_characters(text)
+    print(f"{num_words} words found in the document")
+    print(num_characters)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,15 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_characters(text):
+    char_counts = {}
+    for char in text:
+        lowercase_char = char.lower()
+        if lowercase_char in char_counts:
+            char_counts[lowercase_char] += 1
+        else:
+            char_counts[lowercase_char] = 1
+    
+    return char_counts
