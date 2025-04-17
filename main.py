789dd13efea9395fd141500cba94c7749cb622b7
Ridho M <ridho.mb25@gmail.com>
2025-04-17 10:13:53 +0700

    Print a report

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_on_key
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = count_characters(text)
-    print(f"{num_words} words found in the document")
-    print(num_characters)
+    sorted_num_characters = sort_on_key(num_characters)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...\n")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words\n")
+    print("--------- Character Count -------")
+    for item in sorted_num_characters:
+        # .isalpha used to only print alphabetical characters
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -13,3 +13,19 @@ def count_characters(text):
             char_counts[lowercase_char] = 1
     
     return char_counts
+
+
+def sort_on_key(dict):
+    character_count_list = []
+    for key, value in dict.items():
+        new_dict = {}
+        new_dict["char"] = key
+        new_dict["count"] = value
+        character_count_list.append(new_dict)
+    
+    character_count_list.sort(reverse=True, key=lambda item: item["count"])
+    return character_count_list
+
+
+
+
